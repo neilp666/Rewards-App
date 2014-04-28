60ef9620c95ad924ae233b5c70bbce48a03a96e2
neil patel <neil@myindianweddingapp.com>
2014-04-28 21:35:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rewardsapp::Application.config.secret_key_base = 'a2a22d6707ae6063dd4762752660fb6ef93dd75556d8d7a58818e25e7ac993a71b943943e5a0104491b1a1c4fbf167b4ee642e34f48ae1495bfe5b5d38ad00b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rewardsapp::Application.config.session_store :cookie_store, key: '_rewardsapp_session'
